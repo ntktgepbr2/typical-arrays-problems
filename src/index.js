c31e2ba53cbb816c49a678b46d67244c2ceb61d0
ntktgepbr2 <53575596+ntktgepbr2@users.noreply.github.com>
2020-03-22 13:26:17 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return (
+        array.reduce(function(a, b) {
+            return a + b;
+        }, 0) / array.length
+    );
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+/*exports.min = function min(array) {
+    if (array === 0 || array === []) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
+*/
